406d0a4dc6e93285f9764ed53a7aec4f53085564
Tim Blodgett <mr.tim.blodgett@gmail.com>
2023-11-20 13:48:57 -0700

    Part 6: Adding a New Blog Post Action

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,8 @@ class BlogPostsController < ApplicationController
   rescue
     redirect_to root_path
   end
+
+  def new
+    @blog_post = BlogPost.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "blog_posts#index"
 
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 end
