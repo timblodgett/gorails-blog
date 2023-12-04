3a9d8dd5e6a136af0a4496ac26c3cac1b1a257da
Tim Blodgett <mr.tim.blodgett@gmail.com>
2023-12-04 08:06:05 -0700

    Part 20: Upload Cover Images with ActiveStorage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+  end
 
   # Defines the root path route ("/")
   root "blog_posts#index"
